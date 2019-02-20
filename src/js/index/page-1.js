f9773dcd2f63362663cafb13b5e81e09f3d681cf
momo <1445107381@qq.com>
2019-02-20 11:28:21 +0800

    获取最新音乐列表

--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -13,7 +13,9 @@
         init(view,model){
             this.view = view;
             this.model = model;
-            this.bindEventHub()
+            this.bindEventHub();
+            this.loadModule1();
+            this.loadModule2();
         },
         bindEventHub(){
             window.eventHub.on('changeTab', (tabName) => {
@@ -23,6 +25,16 @@
                     this.view.hide()
                 }
             })
+        },
+        loadModule1(){
+            let script1 = document.createElement('script')
+            script1.src = './js/index/page-1-playlist.js'
+            document.body.appendChild(script1)
+        },
+        loadModule2(){
+            let script2 = document.createElement('script')
+            script2.src = './js/index/page-1-songs.js'
+            document.body.appendChild(script2)
         }
     }
     controller.init(view,model)
